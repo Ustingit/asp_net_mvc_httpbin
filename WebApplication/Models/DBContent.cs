c3dde0361f2b6e6c0445950bbfc12999604ec074
Ustinovich, Yury <y.ustinovich@a1qa.com>
2018-03-21 18:40:19 +0300

    first

---
+++ WebApplication/Models/DBContent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class DBContent
+    {
+        public int Id { get; set; }
+
+        public int CommonResponseTime { get; set; }
+
+        public int DelayedResponseTime { get; set; }
+    }
+}
---
+++ WebApplication/Models/DBContentContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class DBContentContext
+    {
+        public DbSet<DBContent> DBContents { get; set; }
+    }
+}
