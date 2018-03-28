b7763470cbf3e6513b7e0a745e5a8510f5031ebc
Ustinovich, Yury <y.ustinovich@a1qa.com>
2018-03-28 16:44:44 +0300

    move javascript to file

---
+++ WebApplication/Scripts/LineChartScript.js
@@ -0,0 +1,33 @@
+﻿function buildLineChart(firstList, secondList, label, elementId) {
+    
+    var LineChartData =
+            {
+                labels: firstList,
+                datasets: [{
+                    label: label,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderWidth: 2,
+                    data: secondList
+                }]
+            };
+
+    window.onload = function () {
+        var ctx1 = document.getElementById(elementId).getContext("2d");
+        window.myBar = new Chart(ctx1,
+            {
+                type: 'line',
+                data: LineChartData,
+                options:
+                    {
+                        title:
+                        {
+                            display: true,
+                            text: label
+                        },
+                        responsive: true,
+                        maintainAspectRatio: true
+                    }
+            });
+    }
+
+}
