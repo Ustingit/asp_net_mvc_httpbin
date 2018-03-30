5aeeb18429df046a008b6000b394e0219ee7d0a7
Ustinovich, Yury <y.ustinovich@a1qa.com>
2018-03-30 22:54:31 +0300

    ajax databases dirty

--- WebApplication/Controllers/DatabaseResultsController.cs
+++ WebApplication/Controllers/DatabaseResultsController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using WebApplication.Models;
 
@@ -11,8 +13,50 @@ namespace WebApplication.Controllers
         public ActionResult Index()
         {
             IEnumerable<DBContent> contents = db.DBContents;
+            
             ViewBag.Content = contents;
-            return View();
+
+            var delayedResponses = contents.ToList();
+            ViewBag.DR = delayedResponses;
+           
+            return View(delayedResponses);
+        }
+
+        public ActionResult MasterDetailsAjaxHandler()
+        {
+            IEnumerable<DBContent> contents = db.DBContents;
+            var delayedResponses = contents.Where(r => r.DelayedResponseTime != 0).ToList();
+            return Json(new
+            {
+                iTotalRecords = delayedResponses.Count,
+                aaData = delayedResponses
+            },
+            JsonRequestBehavior.AllowGet);
+        }
+        /*
+        public ActionResult AjaxDatabaseResultTable()
+        {
+            IEnumerable<DBContent> contents = db.DBContents;
+            Dictionary<string, List<DBContent>> dict = new Dictionary<string, List<DBContent>>()
+            {
+                contents.Where(r => r.DelayedResponseTime != 0).ToList(),
+                contents.Where(r => r.CommonResponseTime != 0).ToList()
+            };
+            return PartialView(delayedResponses);
+        }*/
+
+        public ActionResult AjaxDatabaseResultTableDelayed()
+        {
+            IEnumerable<DBContent> contents = db.DBContents;
+            var delayedResponses = contents.Where(r => r.DelayedResponseTime != 0).ToList();
+            return PartialView("AjaxDatabaseResultTable", delayedResponses);
+        }
+
+        public ActionResult AjaxDatabaseResultTableCommon()
+        {
+            IEnumerable<DBContent> contents = db.DBContents;
+            var commonResponses = contents.Where(r => r.CommonResponseTime != 0).ToList();
+            return PartialView("AjaxDatabaseResultTable", commonResponses);
         }
     }
 }
