b1b8addb6706b48dfbe2e24a0b9d88d9012a3b7b
Ustinovich, Yury <y.ustinovich@a1qa.com>
2018-04-02 15:44:13 +0300

    fix database results page but css in it yet

--- WebApplication/Controllers/DatabaseResultsController.cs
+++ WebApplication/Controllers/DatabaseResultsController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using WebApplication.Models;
@@ -22,41 +21,18 @@ namespace WebApplication.Controllers
             return View(delayedResponses);
         }
 
-        public ActionResult MasterDetailsAjaxHandler()
-        {
-            IEnumerable<DBContent> contents = db.DBContents;
-            var delayedResponses = contents.Where(r => r.DelayedResponseTime != 0).ToList();
-            return Json(new
-            {
-                iTotalRecords = delayedResponses.Count,
-                aaData = delayedResponses
-            },
-            JsonRequestBehavior.AllowGet);
-        }
-        /*
-        public ActionResult AjaxDatabaseResultTable()
-        {
-            IEnumerable<DBContent> contents = db.DBContents;
-            Dictionary<string, List<DBContent>> dict = new Dictionary<string, List<DBContent>>()
-            {
-                contents.Where(r => r.DelayedResponseTime != 0).ToList(),
-                contents.Where(r => r.CommonResponseTime != 0).ToList()
-            };
-            return PartialView(delayedResponses);
-        }*/
-
         public ActionResult AjaxDatabaseResultTableDelayed()
         {
             IEnumerable<DBContent> contents = db.DBContents;
             var delayedResponses = contents.Where(r => r.DelayedResponseTime != 0).ToList();
-            return PartialView("AjaxDatabaseResultTable", delayedResponses);
+            return PartialView("AjaxDatabaseResultTableDelayed", delayedResponses);
         }
 
         public ActionResult AjaxDatabaseResultTableCommon()
         {
             IEnumerable<DBContent> contents = db.DBContents;
             var commonResponses = contents.Where(r => r.CommonResponseTime != 0).ToList();
-            return PartialView("AjaxDatabaseResultTable", commonResponses);
+            return PartialView("AjaxDatabaseResultTableCommon", commonResponses);
         }
     }
 }
