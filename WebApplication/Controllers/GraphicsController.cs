2a9ac91f4704afe13a2007c51bfec93159d200f8
Ustinovich, Yury <y.ustinovich@a1qa.com>
2018-04-03 14:48:27 +0300

    add ajax chart building but without autorefresh

--- WebApplication/Controllers/GraphicsController.cs
+++ WebApplication/Controllers/GraphicsController.cs
@@ -24,5 +24,30 @@ namespace WebApplication.Controllers
             
             return View();
         }
+
+        [HttpPost]
+        public ActionResult AjaxChart()
+        {
+            IEnumerable<DBContent> contents = db.DBContents;
+
+            var delayedResponses = contents.Where(r => r.DelayedResponseTime != 0)
+                                           .Select(x => x.DelayedResponseTime);
+            var commonResponses = contents.Where(r => r.CommonResponseTime != 0)
+                                          .Select(x => x.CommonResponseTime);
+
+            var ContentIds = new List<int>();
+            for (var i = 1; i <= delayedResponses.Count(); i++)
+            {
+                ContentIds.Add(i);
+            }
+
+            var result = new
+            {
+                DelayedResponseTimes = delayedResponses.ToList(),
+                CommonResponseTimes = commonResponses.ToList(),
+                ContentIds = ContentIds
+            };
+            return Json(result);
+        }
     }
 }
