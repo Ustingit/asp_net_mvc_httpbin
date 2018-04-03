da2e8e4bd449ccefae1f50d080a83eeef3056537
Ustinovich, Yury <y.ustinovich@a1qa.com>
2018-04-03 15:17:14 +0300

    refresh without page resfreshing work, but too broad

--- WebApplication/Controllers/GraphicsController.cs
+++ WebApplication/Controllers/GraphicsController.cs
@@ -49,5 +49,29 @@ namespace WebApplication.Controllers
             };
             return Json(result);
         }
+
+        public ActionResult AjaxPartial()
+        {
+            IEnumerable<DBContent> contents = db.DBContents;
+
+            var delayedResponses = contents.Where(r => r.DelayedResponseTime != 0)
+                                           .Select(x => x.DelayedResponseTime);
+            var commonResponses = contents.Where(r => r.CommonResponseTime != 0)
+                                          .Select(x => x.CommonResponseTime);
+
+            var ContentIds = new List<int>();
+            for (var i = 1; i <= delayedResponses.Count(); i++)
+            {
+                ContentIds.Add(i);
+            }
+
+            var result = new
+            {
+                DelayedResponseTimes = delayedResponses.ToList(),
+                CommonResponseTimes = commonResponses.ToList(),
+                ContentIds = ContentIds
+            };
+            return PartialView("AjaxChart", Json(result));
+        }
     }
 }
